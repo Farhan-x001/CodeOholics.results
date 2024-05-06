f82f6f1f015fd2c13448a4f38637749fcde8b7e8
Farhan-x001 <115449922+Farhan-x001@users.noreply.github.com>
2024-05-06 17:30:00 +0530

    fixed landing

--- pages/index.js
+++ pages/index.js
@@ -7,24 +7,16 @@ import Image from 'next/image';
 const LandingPage = () => {
   return (
     <div className={styles.body}>
-      <div className={styles.healthkare}>HealthKare.AI</div>
+      <div className={styles.healthkare}>CodeOholics </div>
     <div className={styles.centered}>
-      <h1 className={styles.brandLabel}>Select your role</h1>
-      <p className={styles.para}>To move forward you have to select your role</p>
+      <h1 className={styles.brandLabel}>Membership drive Results-2k24</h1>
+      <p className={styles.para}>please click down to view details</p>
       <div className={styles.linksContainer}>
         <div className={styles.linkBox}>
           <Link href="/home">
           <div className ={styles.img1}>
           <Image src="/images/doctorsicon.png" alt="Hospital Icon" width={100} height={100} className={styles.image1} />
-              <h4>Doctors</h4>
-            </div>
-          </Link>
-        </div>
-        <div className={styles.linkBox}>
-          <Link href="/home">
-            <div className ={styles.img2}>
-            <Image src="/images/hospital-icon.png" alt="Hospital Icon" width={100} height={100}  />
-              <h4>Hospital</h4>
+              <h4>Results</h4>
             </div>
           </Link>
         </div>
